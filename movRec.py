9d32b246064fb8e677de5c40ee5ba994c1c0607e
techoholic <nicolowheaton@gmail.com>
2020-06-03 17:34:09 -0700

    Fix movRec.py to work

--- movRec.py
+++ movRec.py
@@ -6,11 +6,11 @@ print("Recording session started. Press ` to stop")
 movSeq = keyboard.record(until='`')
 print("Recording session ended. What do you want to do now?")
 option = ''
-while (!option):
+while (option = ''):
     print("p. Playback you recording")
     print("s. Save the recording as a file to be accessed later")
     print("x. Delete the recording")
     print("`. Discard your recording and start a new one")
-    option = input("Type p, s, x, or ` (tilde) and then press enter")
-    if (option == 'p'):
+    option = input("Type p, s, x, or ` (tilde) and then press enter: ")
+    if (option == '``p' or option == 'p'):
         keyboard.play(movSeq)
