c5fcf5ac72af0ba7ebfeb0c4130982c168f5586e
techoholic <nicolowheaton@gmail.com>
2020-06-03 17:25:26 -0700

    Update movRec.py with options and add .gitignore

--- movRec.py
+++ movRec.py
@@ -1,10 +1,16 @@
 import keyboard
-import
-print("This scripts records your movements and saves them in a '.txt' file")
+print("This scripts records your movement sequences and saves them in a file")
 print("Press ` to start and then again to stop")
 keyboard.wait('`')
-print("Recording session started. Type away!")
-movement = keyboard.record(until='`')
-print("Recording session ended. Press 'space' to play back the movement sequence")
-keyboard.wait('space')
-keyboard.play(movement)
+print("Recording session started. Press ` to stop")
+movSeq = keyboard.record(until='`')
+print("Recording session ended. What do you want to do now?")
+option = ''
+while (!option):
+    print("p. Playback you recording")
+    print("s. Save the recording as a file to be accessed later")
+    print("x. Delete the recording")
+    print("`. Discard your recording and start a new one")
+    option = input("Type p, s, x, or ` (tilde) and then press enter")
+    if (option == 'p'):
+        keyboard.play(movSeq)
