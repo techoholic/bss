529df460840358ad89b040bd2fa68edd5e85c14a
techoholic <nicolowheaton@gmail.com>
2020-06-05 07:25:59 -0700

    Update sequencer.py to v1.5: time attr and overall fixes (big update)

--- sequencer.py
+++ sequencer.py
@@ -1,10 +1,11 @@
 import keyboard
 import time
-print("Welcome to version 1.1 of the Sequencer! This script can record, playback, and most importantly, sequence\n keypress recordings (called movement sequences aka movSeqs).")
+import os
+print("Welcome to version 1.1 of the Sequencer! This script can record, playback, and most importantly, sequence keypress recordings called Python Keyboard Recordings (.pkr files).")
 while True:
-    print("\na. Record new movSeq")
-    print("b. Playback a recorded movSeq")
-    print("c. Sequence the sequences!")
+    print("\na. Record new PKR")
+    print("b. Playback a recorded PKR")
+    print("c. Sequence the PKRs (doesn't work yet!)")
     option = input("Type a, b, or c and press Enter: ")
     if (option == 'a'):
         print("\nRecording will start in 5")
@@ -18,7 +19,7 @@ while True:
         print("1")
         time.sleep(1)
         print("Recording session started. Press ` to stop")
-        movSeq = keyboard.record(until='`')
+        pkr = keyboard.record(until='`')
         print("\nRecording session ended. What do you want to do now?")
         working = True
         option = ''
@@ -27,15 +28,15 @@ while True:
             if (firstTime == False and option != 'k'):
                 print("\nWhat now?")
             firstTime = False
-            print("i. Playback your recording")
+            print("i. Playback the recording")
             print("j. Save the recording as a file to be accessed later")
-            print("k. Discard your recording and start a new one")
+            print("k. Discard the recording and start a new one")
             print("l. Return to main menu")
             option = input("Type i, j, k, or l and then press enter: ")
             if (option == '`i' or option == 'i'):
                 loop = input("\nDo you want to play the recording on loop? (y/n): ")
                 running = True
-                print("Playing the movSeq in 5")
+                print("Playing the PKR in 5")
                 time.sleep(1)
                 print("4")
                 time.sleep(1)
@@ -45,18 +46,23 @@ while True:
                 time.sleep(1)
                 print("1")
                 time.sleep(1)
-                print("Playing movement sequence")
+                print("Playing recording")
                 while (running):
                     if (option == 'n'):
                         running = False
-                    keyboard.play(movSeq)
+                    keyboard.play(pkr)
             elif (option == '`j' or option == 'j'):
-                name = input("What do you want to name this movSeq?: ")
-                file = open("local/movSeqs/" + name, "w")
-                file.writelines(str(movSeq))
+                name = input("\nWhat do you want to name this PKR?: ")
+                file = open("local/" + name + ".pkr", "w")
+                pkrFileArray = []
+                for entry in pkr:
+                    pkrFileArray.append(str(entry))
+                    pkrFileArray.append(str(getattr(entry, "time")))
+                for entry in pkrFileArray:
+                    file.write(entry + "\n")
                 file.close()
             elif (option == '`k' or option == 'k'):
-                print("Recording will start in 5")
+                print("\nRecording will start in 5")
                 time.sleep(1)
                 print("4")
                 time.sleep(1)
@@ -68,4 +74,27 @@ while True:
                 time.sleep(1)
                 print("Recording session started. Press ` to stop")
                 movSeq = keyboard.record(until='`')
-                print("Recording session ended. What do you want to do now?")
+                print("\nRecording session ended. What do you want to do now?")
+    elif (option == 'b'):
+        fileList = os.listdir("local/")
+        i = 0
+        for fileName in fileList:
+            print(i, ". ", fileName, sep='')
+            i = i + 1
+        fto = input("Type the number of the PKR you want to play and press Enter: ") #file to open
+        fileName = fileList[int(fto)]
+        file = open("local/"+ fileName, 'r')
+        pkrFromFile = file.readlines()
+        file.close()
+        pkrToPlay = []
+        i = 0
+        class KeyboardEvent:
+            def __init__(self, time):
+                self.time = time
+        for entry in pkrFromFile:
+            if (i%2 == 0):
+                pkrToPlay.append(entry)
+            else:
+                setattr(pkrToPlay[i-1], "time", float(entry))
+            i = i + 1
+        keyboard.play(pkrToPlay)
